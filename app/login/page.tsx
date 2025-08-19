386fefc0034f6bbdbe58b0253b3c26ae013b16f3
Daniil Shcherbakov <djdaniil46@gmail.com>
2025-08-19 08:17:57 +0200

    fixes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,14 @@
 import LoginForm from "@/components/auth/login-form"
+import { Suspense } from "react"
 
 const LoginPage = () => {
-  return (
-    <div className="flex justify-center items-center h-full"><LoginForm/></div>
-  )
+    return (
+        <div className="flex justify-center items-center h-full">
+            <Suspense fallback={<div className="text-center">Loading...</div>}>
+                <LoginForm />
+            </Suspense>
+        </div>
+    )
 }
 
 export default LoginPage
